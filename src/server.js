e6f11c60bc4ade1497027bad9e1eb8ba3b366f98
feebrian <adamaditama13@gmail.com>
2023-12-12 23:31:40 +0700

    restructure hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const NotesService = require("./services/inMemory/NotesService");
+const notes = require("./api/notes");
 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: 3000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
